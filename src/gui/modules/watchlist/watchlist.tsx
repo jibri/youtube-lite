1af2bd578763f3c4dbb0a7b1f7af558c88b1ea6d
Jibri <jeremie.briand@outlook.fr>
2022-12-10 19:38:17 +0100

    animation video

--- src/gui/modules/watchlist/watchlist.tsx
+++ src/gui/modules/watchlist/watchlist.tsx
@@ -68,7 +68,7 @@ function Watchlist() {
         })
         .then(() => deleteFromWatchlist(video.playlistItem.id), handleError)
         .then(() => incLoading(-1));
-    }, 500);
+    }, 1000);
   };
 
   const likeVideo = (video: VideoItem) => {
